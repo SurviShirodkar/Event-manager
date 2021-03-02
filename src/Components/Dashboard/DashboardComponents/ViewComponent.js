c9134ba7ad69a5ebb0643dec8c6cea6d875b2147
survi shirodkar <survi@bluestream.io>
2021-03-02 23:43:51 +0530

    changes

--- src/Components/Dashboard/DashboardComponents/ViewComponent.js
+++ src/Components/Dashboard/DashboardComponents/ViewComponent.js
@@ -1,15 +1,38 @@
-import React from 'react'
-import './viewcomponent.scss'
+import React from 'react'
+import './viewcomponent.scss'
 
-export default function ViewComponent({userList}) {
-    React.useEffect(() => {
-        console.log("userlist",userList)
-    },[])
-    return (
-       
-        <div>
-            { userList.map(obj => <div>{obj.email}</div>
-            )}
-        </div>
-    )
+export default function ViewComponent({userList}) {
+    
+    React.useEffect(() => {
+        console.log("userlist",userList)
+    },[])
+    return (
+       
+        <div className="viewComponentMain ">
+            <table className="table ">
+                <thead>
+                    <tr className="">
+                        <th className="p-0"> FIRSTNAME</th>
+                        <th>LASTNAME</th>  
+                        <th>CONTACT NUMBER</th>  
+                        <th>GENDER</th>  
+                        <th>Email</th>  
+                        <th>EVENT</th>  
+ </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>{ userList.map(obj => <div>{obj.firstname}</div>)}</th>
+                        <th>{ userList.map(obj => <div>{obj.lastname}</div>)}</th>
+                        <th>{ userList.map(obj => <div>{obj.contactnumber}</div>)}</th>
+                        <th>{ userList.map(obj => <div>{obj.gender}</div>)}</th>
+                        <th>{ userList.map(obj => <div>{obj.email}</div>)}</th>
+                        <th>{ userList.map(obj => <div>{obj.event}</div>)}</th>
+                    </tr>
+                </tbody>
+            </table>
+          
+            <div className=""></div>
+        </div>
+    )
 }
