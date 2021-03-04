b706cae8d904aeeb8edbab26a3cd66a15c4e3ab2
survi shirodkar <survi@bluestream.io>
2021-03-04 17:28:01 +0530

    changes

--- src/Components/Dashboard/DashboardComponents/AnalysisComponent.js
+++ src/Components/Dashboard/DashboardComponents/AnalysisComponent.js
@@ -4,11 +4,77 @@ import './analysiscomponent.scss'
 export default function AnalysisComponent({userList,setUserList}) {
 
   const [modalState, setModalState] = React.useState(false)
+  
+  const [editDetails, setEditDetails] = React.useState(
+    {
+        firstname: "",
+        lastname: "",
+        contactnumber: "",
+        gender: "",
+        email: "",
+        event: "",
+    }
+  )
+  const deleteRow = (num) =>{
+  
+   userList.splice(num,1)
+   setUserList(userList)
+  };
     return (
         <div className="analysis-main">
           <div className={modalState?"overlay":"d-none"}>
             <button onClick={()=>setModalState(false)} className="close-button">X</button>
             <div className="modal-main">
+            <input
+                type="text"
+                className="form-control"
+                 placeholder="Shailesh"
+                value={editDetails.firstname}
+              ></input>
+               <input
+                type="text"
+                className="form-control"
+                 placeholder="haldankar"
+                 value={editDetails.lastname}
+              ></input>
+                 <input
+                type="text"
+                className="form-control"
+                 placeholder="7768076298"
+                 value={editDetails.contactnumber}
+                 ></input>
+
+                 <div className="form-group mt-3">
+                <label className="mr-3 ">Gender :</label>
+                <select
+                  className="custom-select"
+                  value={editDetails.gender}
+                
+                >
+                  <option selected>Choose...</option>
+                  <option value="male">male</option>
+                  <option value="female">female</option>
+                  <option value="others">others</option>
+                </select>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                 placeholder="survi@gmail.com"
+                 value={editDetails.email}
+                 ></input>
+                  <div className="form-group mt-3">
+                <label className="mr-3 ">Event :</label>
+                <select
+                  className="custom-select"
+                  value={editDetails.event}
+                >
+                  <option selected>Choose...</option>
+                  <option value="happenings">happenings</option>
+                  <option value="fact">fact</option>
+                  <option value="others">others</option>
+                </select>
+              </div>
 
             </div>
 
@@ -28,7 +94,7 @@ export default function AnalysisComponent({userList,setUserList}) {
             </thead>
             <tbody>
                 {
-                    userList.map(obj =><tr>
+                    userList.map((obj,num) =><tr>
 
                     <td>{obj.firstname}</td>
                     <td>{obj.lastname}</td>
@@ -36,8 +102,8 @@ export default function AnalysisComponent({userList,setUserList}) {
                     <td>{obj.gender}</td>
                     <td>{obj.email}</td>
                     <td>{obj.event}</td>
-                    <td><button onClick={()=>setModalState(true)} >E</button></td>
-                    <td><button  className="analysis-delete">D</button></td>
+                    <td><button onClick = {() =>{setModalState(true) ; setEditDetails(obj)}} >E</button></td>
+                    <td><button onClick = {()=>deleteRow(num)}  className="analysis-delete">D</button></td>
                     </tr>)
                 }
 
